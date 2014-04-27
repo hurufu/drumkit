61b7b8a7ad58d33277efb4109d79c9d60ab62681
Aleksy Grabowski <hurufu@gmail.com>
2014-04-27 21:17:15 +0200

    Code refactored

--- main.cpp
+++ main.cpp
@@ -25,83 +25,44 @@
 #define ERR_BAD_ARGUMENT 0xfb
 #define ERR_UNKNOWN 0xfa
 
-// Error handling variables
+// Program state flags
 int r = SUCCESS;
+int verbatim = 0; 
+bool run = true;
+
+struct jack_callback_arg {
+    jack_port_t * output_port;
+    char pad_map[NOF_PADS];
+    bool pad_states[NOF_PADS];
+    int loop_buffer;
+};
 
 libusb_device_handle * open_device(int vendor, int product);
-static int process(jack_nframes_t nframes, void *arg);
+static int process(jack_nframes_t nframes, void * arg);
+static void signal_handler(int);
+void option_handler(int ac, char ** av, jack_callback_arg * drumkit_callback);
 
-static void sigexit(int);
 
-libusb_device_handle * usb_drumkit_handle = NULL;
-jack_client_t * jack_midi_drum = NULL;
-jack_port_t * output_port;
-volatile unsigned char raw[8];
-
-bool pad[6];
-bool was_free[6];
-
-int verbatim = 0;
-
-char pad_map[NOF_PADS] = {0x20, 0x21, 0x22, 0x23, 0x24, 0x25};
-
-int main (int ac, char* av[])
+int main (int ac, char ** av)
 {
     int transfered;
     jack_nframes_t nframes;
     const char jack_midi_drum_str[] = "Dreamlink Foldup Drumk Kit";
     char * pad_str = NULL;
     
-    int buffer = 0;
+    libusb_device_handle * usb_drumkit_handle = NULL;
+    jack_client_t * jack_midi_drum = NULL;
+    volatile unsigned char raw[8];
     
-    //BEGIN getopt
-    //FIXME I dont have enough stamina to write getopt, and I don't want getopt
-    // to be larger than actual driver! I have a plan 'bout ultimate™ getopt, 
-    // but I don't think I'll make it soon... And yeah next routine is 
-    // dnagerous, but I like danger :P.
-    char c;
-    int p_tmp = -1;
-    while ((c = getopt (ac, av, "hv:p:n:b:")) != -1) {
-        switch (c) {
-            case 'h':
-                std::cout << "!!WARNING CODE IS VERY UNMATURE, INCORRECT VALUES WILL CAUSE UNPREDICTABLE RESULTS!!\n"
-                    << "Usage:\n"
-                    << "\t-h\t\tOutput this message and exit\n"
-                    << "\t-v <level>\tVerbosity level (default 0)\n"
-                    << "\t-p <number>\tAsign to pad 0..5\n"
-                    << "\t-n <note>\tnote in range 0..127\n"
-                    << "\t-b <integer>\t Select size of buffer (default 0)\n";
-                return 0;
-            case 'v':
-                verbatim = atoi(optarg);
-                break;
-            case 'p':
-                p_tmp = atoi(optarg);
-                break;
-            case 'n':
-                pad_map[p_tmp]=atoi(optarg);
-                break;
-            case 'b':
-                buffer = atoi(optarg);
-                break;
-            case '?':
-                return ERR_BAD_ARGUMENT;
-            default:
-                abort();
-        }
-    }
-    //END getopt
+    struct jack_callback_arg drumkit_callback;
+    for (int i=0; i < NOF_PADS; i++)
+        drumkit_callback.pad_map[i] = 0x20 + i;
+    for (int i=0; i < NOF_PADS; i++)
+        drumkit_callback.pad_states[i] = false;
+    drumkit_callback.loop_buffer = 0;
     
-    if (verbatim)
-        std::cout << "Selected buffer: " << buffer << std::endl;
+    option_handler(ac, av, &drumkit_callback);
     
-    if (p_tmp != -1) {
-        std::cout << "Reassigned mapping:";
-        for(int q=0; q<NOF_PADS; q++)
-            std::cout << "\tPad" << q << ": " << (int) pad_map[q];
-        std::cout << std::endl;
-    }
-
     r = libusb_init(NULL);
     if (r != SUCCESS) {
         std::cerr << "libusb_init: " << libusb_strerror((enum libusb_error)r) << "\n";
@@ -137,12 +98,9 @@ int main (int ac, char* av[])
         libusb_exit(NULL);
         return ERR_JACK_SPECIFIC;
     }
-
-    jack_set_process_callback(jack_midi_drum, process, 0);
-
-    output_port = jack_port_register (jack_midi_drum, "out", JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
+    drumkit_callback.output_port = jack_port_register(jack_midi_drum, "out", JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
     nframes = jack_get_buffer_size(jack_midi_drum);
-
+    jack_set_process_callback(jack_midi_drum, process, &drumkit_callback);
     r = jack_activate(jack_midi_drum);
     if (r != SUCCESS) {
         std::cerr << "Cannot activate client --- " << r << "\n";
@@ -152,14 +110,14 @@ int main (int ac, char* av[])
         return ERR_JACK_SPECIFIC;
     }
     
-    signal(SIGQUIT, sigexit);
-    signal(SIGHUP,  sigexit);
-    signal(SIGTERM, sigexit);
-    signal(SIGINT,  sigexit);
+    signal(SIGQUIT, signal_handler);
+    signal(SIGHUP,  signal_handler);
+    signal(SIGTERM, signal_handler);
+    signal(SIGINT,  signal_handler);
     
-    while(true) {
+    while(run) {
         unsigned char p = 0;
-        for(int i = 0; i <= buffer; i++)
+        for(int i = 0; i <= drumkit_callback.loop_buffer; i++)
         {
             r = libusb_interrupt_transfer(usb_drumkit_handle, 0x81, (unsigned char*) raw, sizeof(raw), &transfered, 0);
             if(r >= 0) {
@@ -178,7 +136,7 @@ int main (int ac, char* av[])
             }
         }
         for (int i = 0; i < NOF_PADS; i++) {
-            pad[i] = p & 1;
+            drumkit_callback.pad_states[i] = p & 1;
             p >>= 1;
         }
     }
@@ -192,39 +150,83 @@ int main (int ac, char* av[])
     return r;
 }
 
-static void sigexit(int sig)
+void option_handler(int ac, char ** av, jack_callback_arg * drumkit_callback)
 {
-    jack_deactivate(jack_midi_drum);
-    jack_client_close(jack_midi_drum);
-    libusb_release_interface(usb_drumkit_handle, 0);
-    libusb_close(usb_drumkit_handle);
-    libusb_exit(NULL);
-
-    std::cerr << "Signal " << sig << " received, exiting ...\n";
-    exit(0);
+    //FIXME I dont have enough stamina to write getopt, and I don't want getopt
+    // to be larger than actual driver! I have a plan 'bout ultimate™ getopt, 
+    // but I don't think I'll make it soon... And yeah next routine is 
+    // dnagerous, but I like danger :P.
+    char c;
+    int p_tmp = -1;
+    while ((c = getopt (ac, av, "hv:p:n:b:")) != -1) {
+        switch (c) {
+            case 'h':
+                std::cout << "!!WARNING CODE IS VERY UNMATURE, INCORRECT VALUES WILL CAUSE UNPREDICTABLE RESULTS!!\n"
+                    << "Usage:\n"
+                    << "\t-h\t\tOutput this message and exit\n"
+                    << "\t-v <level>\tVerbosity level (default 0)\n"
+                    << "\t-p <number>\tAsign to pad 0..5\n"
+                    << "\t-n <note>\tnote in range 0..127\n"
+                    << "\t-b <integer>\t Select size of loop_buffer (default 0)\n";
+                exit(0);
+            case 'v':
+                verbatim = atoi(optarg);
+                break;
+            case 'p':
+                p_tmp = atoi(optarg);
+                break;
+            case 'n':
+                drumkit_callback->pad_map[p_tmp]=atoi(optarg);
+                break;
+            case 'b':
+                drumkit_callback->loop_buffer = atoi(optarg);
+                break;
+            case '?':
+                exit(ERR_BAD_ARGUMENT);
+            default:
+                abort();
+        }
+    }
+    if (verbatim)
+        std::cout << "Selected loop_buffer: " << drumkit_callback->loop_buffer << std::endl;
+    
+    if (p_tmp != -1) {
+        std::cout << "Reassigned mapping:";
+        for(int q=0; q<NOF_PADS; q++)
+            std::cout << "\tPad" << q << ": " << (int) drumkit_callback->pad_map[q];
+        std::cout << std::endl;
+    }
 }
 
-static int process(jack_nframes_t nframes, void *arg)
+static void signal_handler(int sig)
 {
-    void * port_buf = jack_port_get_buffer(output_port, nframes);
+    run = false;
+    std::cerr << "Signal " << sig << " received, exiting ..." << std::endl;
+}
+
+static int process(jack_nframes_t nframes, void * arg)
+{
+    jack_callback_arg drumkit_callback = *(jack_callback_arg*) arg;
+    void * port_buf = jack_port_get_buffer(drumkit_callback.output_port, nframes);
     unsigned char * midi_buffer;
     jack_midi_clear_buffer(port_buf);
+    static bool was_free[6];
     
     for (int i = 0; i < 6; i++)
-    if (pad[i]) {
+    if (drumkit_callback.pad_states[i]) {
         if (was_free[i]) {
             was_free[i] = false;
             midi_buffer = jack_midi_event_reserve(port_buf, 0, 3);
-            midi_buffer[0] = 0x9A;
-            midi_buffer[1] = pad_map[i];
+            midi_buffer[0] = 0x99;
+            midi_buffer[1] = drumkit_callback.pad_map[i];
             midi_buffer[2] = 0x7F;
         }
     } else {
         if (!was_free[i]) {
             was_free[i] = true;
             midi_buffer = jack_midi_event_reserve(port_buf, 0, 3);
-            midi_buffer[0] = 0x8A;
-            midi_buffer[1] = pad_map[i];
+            midi_buffer[0] = 0x89;
+            midi_buffer[1] = drumkit_callback.pad_map[i];
             midi_buffer[2] = 0x00;
         }
     }
@@ -237,7 +239,7 @@ libusb_device_handle * open_device(int vendor, int product)
     libusb_device ** devices;
     libusb_device_descriptor desc;
     libusb_config_descriptor * config;
-    libusb_device_handle * usb_drumkit_handle;
+    libusb_device_handle * devh;
     int r;
 
     int cnt = libusb_get_device_list(NULL, &devices);
@@ -258,7 +260,7 @@ libusb_device_handle * open_device(int vendor, int product)
                 return NULL;
             }
             
-            r = libusb_open(devices[i], &usb_drumkit_handle);
+            r = libusb_open(devices[i], &devh);
             if (r < 0) {
                 std::cerr << "libusb_open: " << libusb_strerror((enum libusb_error)r) << "\n";
                 return NULL;
@@ -266,7 +268,7 @@ libusb_device_handle * open_device(int vendor, int product)
         }
     }
     libusb_free_device_list(devices, 1);
-    return usb_drumkit_handle;
+    return devh;
 }
 
 
